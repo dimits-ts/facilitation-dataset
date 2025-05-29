49c0e667e0c2bd07a5e398bee86356bfaf1a4005
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-05-29 13:50:25 +0300

    complete cmv-awry2

--- scripts/cmv-awry2.sh
+++ scripts/cmv-awry2.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
-
-DATA_PATH="./datasets/cmv_awry2/"
-INTERMEDIATE_DIR="./datasets/cmv_awry_tmp"
-DOWNLOAD_DIR="./downloads"
-
+DOWNLOAD_DIR="downloads/cmv_awry2"
 DOWNLOAD_PATH="$DOWNLOAD_DIR/cmv_awry2.zip"
-URL="https://figshare.com/ndownloader/articles/21077989/versions/1"
 
+URL="https://figshare.com/ndownloader/articles/21077989/versions/1"
+  
 # what the hell is this dataset
 export UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE
-
-mkdir -p $DATA_PATH $DOWNLOAD_PATH
-if [ ! -f "$DOWNLOAD_PATH" ]; then
+   
+mkdir -p $(dirname $DOWNLOAD_PATH)
+  
+if [ ! -f $DOWNLOAD_PATH ]; then
   echo "Downloading..."
-  wget -O "$DOWNLOAD_PATH" "$URL"
+  wget -O $DOWNLOAD_PATH $URL
 else
   echo "Download already exists. Skipping download."
 fi
-
+  
 echo "Extracting..."
-# intermediate .gz files go here
-unzip -u $DOWNLOAD_PATH -d $INTERMEDIATE_DIR
-
-for file in "$INTERMEDIATE_DIR"/*; do 
+unzip -u $DOWNLOAD_PATH -d $DOWNLOAD_DIR
+for file in `find . -type f -name "*.gz"`; do
     echo "Unzipping $file..."
-    STEM=$(basename "${file}.gz")
-    gunzip -c "${file}" > "$DATA_PATH/${STEM}.json" 
+    STEM=$(basename "${file}" .gz)
+    OUTPUT_FILE="$DOWNLOAD_DIR/$STEM.json"
+    touch $OUTPUT_FILE
+    gunzip -c "${file}" > $OUTPUT_FILE
 done
-
-# cleanup
-#rm -r $INTERMEDIATE_DIR
-
 echo "Finished"
--- scripts/wikidisputes.sh
+++ scripts/wikidisputes.sh
@@ -1,8 +1,6 @@
-DATA_DIR="./datasets"
-DOWNLOAD_DIR="./downloads"
-DATA_PATH="$DATA_DIR/wikidisputes/"
+DOWNLOAD_DIR="downloads/wikidisputes"
 DOWNLOAD_PATH="$DOWNLOAD_DIR/wikidisputes.tar.gz"
 
-mkdir -p $DATA_PATH
+mkdir -p $DOWNLOAD_DIR
 wget -nc -O $DOWNLOAD_PATH "https://github.com/christinedekock11/wikidisputes/raw/refs/heads/main/data.tar.gz"
-tar -xvzf $DOWNLOAD_PATH -C $DATA_PATH
+tar -xvzf $DOWNLOAD_PATH -C $DOWNLOAD_DIR
