d68d38ec03ac688d77795c40ba56a160859051f0
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-06-04 13:28:28 +0300

    implement parallel unzipping for large datasets

--- scripts/cmv-awry2.sh
+++ scripts/cmv-awry2.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
+
 DOWNLOAD_DIR="downloads/cmv_awry2"
 MASTER_ZIP_PATH="$DOWNLOAD_DIR/cmv_awry2.zip"
 URL="https://figshare.com/ndownloader/articles/21077989/versions/1"
 
-
+# Check if .json files already exist
 json_file_count=$(find "$DOWNLOAD_DIR" -maxdepth 1 -type f -iname '*.json' | wc -l)
 if [ $json_file_count -ge 5 ]; then
     echo "All .json files already there. Exiting."
     exit 0
 fi
 
-  
-# what the hell is this dataset
+# Disable unzip zipbomb protection
 export UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE
-   
-mkdir -p $(dirname $MASTER_ZIP_PATH)
-  
+
+mkdir -p "$(dirname "$MASTER_ZIP_PATH")"
+
 echo "Downloading..."
-wget -nc -O $MASTER_ZIP_PATH $URL
+wget -nc -O "$MASTER_ZIP_PATH" "$URL"
 
-echo "Extracting..."
-unzip -u $MASTER_ZIP_PATH -d $DOWNLOAD_DIR
-rm $MASTER_ZIP_PATH # remove large .zip file
+echo "Extracting master zip..."
+unzip -u "$MASTER_ZIP_PATH" -d "$DOWNLOAD_DIR"
+rm "$MASTER_ZIP_PATH"
 
-for file in `find . -type f -name "*.gz"`; do
-  STEM=$(basename "${file}" .gz)
-  OUTPUT_FILE="$DOWNLOAD_DIR/$STEM.json"
-  if [! -f $OUTPUT_FILE]; then
-    echo "Unzipping $file..."
-    touch $OUTPUT_FILE
-    gunzip -c $file > $OUTPUT_FILE
-    rm $file # remove intermediate .gz archive
-  else
-    echo "File $OUTPUT_FILE already exits, skipping..."
-  fi
-done
+# Parallel gunzip extraction using xargs
+find "$DOWNLOAD_DIR" -type f -name "*.gz" | \
+  xargs -P 4 -I{} bash -c '
+    file="{}"
+    stem=$(basename "$file" .gz)
+    output_file="'$DOWNLOAD_DIR'/$stem.json"
+    if [ ! -f "$output_file" ]; then
+        echo "Unzipping $file..."
+        gunzip -c "$file" > "$output_file"
+        rm "$file"
+    else
+        echo "File $output_file already exists, skipping..."
+    fi
+'
 
 echo "Finished"
--- scripts/wikiconv.sh
+++ scripts/wikiconv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 DOWNLOAD_DIR="downloads/wikiconv"
 ZIP_ROOT="$DOWNLOAD_DIR/datasets/wikiconv-corpus/corpus-zipped"
 URL="https://zissou.infosci.cornell.edu/convokit/datasets/wikiconv-corpus/"
-
 
 # Check if 18 directories exist and match wikiconv-2001 to wikiconv-2018
 EXPECTED_DIRS=18
@@ -12,19 +12,14 @@ if [ "$actual_count" -eq "$EXPECTED_DIRS" ]; then
     exit 0
 fi
 
+mkdir -p "$DOWNLOAD_DIR"
+wget -nc -r -nH --no-parent --cut-dirs=1 -P "$DOWNLOAD_DIR" "$URL" -A .zip
 
-mkdir -p $DOWNLOAD_DIR
-wget -nc -r -nH --no-parent --cut-dirs=1 -P $DOWNLOAD_DIR $URL -A .zip
+# Parallel unzipping using xargs with 4 parallel processes (you can adjust -P)
+find "$ZIP_ROOT" -type f -name "full.corpus.zip" | \
+  xargs -P 4 -I{} unzip {} -q -d "$DOWNLOAD_DIR"
 
-# for each directory in the directory ./datasets/wikiconv-corpus/corpus-zipped
-#   unzip full.corpus.zip
-#   move unzipped contents to ./wikiconv
+mv "$DOWNLOAD_DIR/datasets/wikiconv-corpus/blocks.json" "$DOWNLOAD_DIR"
 
-find "$ZIP_ROOT" -type f -name "full.corpus.zip" | while read zip_file; do
-    unzip $zip_file -d $DOWNLOAD_DIR
-done
-
-mv "$DOWNLOAD_DIR/datasets/wikiconv-corpus/blocks.json" $DOWNLOAD_DIR
-
-# remove .zip directories
+# Cleanup
 rm -r "$DOWNLOAD_DIR/datasets"
