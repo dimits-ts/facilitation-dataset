83234602cfc7ee507ace1d668102ee0da2b9e731
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-06-23 10:58:15 +0300

    fix unzip bug

--- scripts/wikiconv.sh
+++ scripts/wikiconv.sh
@@ -15,11 +15,10 @@ fi
 mkdir -p "$DOWNLOAD_DIR"
 wget --no-verbose -nc -r -nH --no-parent --cut-dirs=1 -P "$DOWNLOAD_DIR" "$URL" -A .zip
 
-# Parallel unzipping using xargs with 4 parallel processes (you can adjust -P)
+echo "Unzipping datasets..."
 find "$ZIP_ROOT" -type f -name "full.corpus.zip" | \
-  xargs -P 4 -I{} unzip {} -q -d "$DOWNLOAD_DIR"
+  xargs -P 4 -I{} unzip -q -d "$DOWNLOAD_DIR" "{}"
+echo "Done."
 
 mv "$DOWNLOAD_DIR/datasets/wikiconv-corpus/blocks.json" "$DOWNLOAD_DIR"
 
-# Cleanup
-rm -r "$DOWNLOAD_DIR/datasets"
