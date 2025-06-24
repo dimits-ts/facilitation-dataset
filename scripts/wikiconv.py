6a7c81776c9b09da11f2f3d2f47c5d61b7383c44
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-06-24 16:31:23 +0300

    enable english detection

--- scripts/wikiconv.py
+++ scripts/wikiconv.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import uuid
 
 import pandas as pd
+import py3langid as langid
 from tqdm.auto import tqdm
 
 from tasks import preprocessing_util
@@ -33,17 +35,25 @@ def main():
 
 def process_dataset(df):
     df = df.dropna(subset=["text"])
+    # masked IP addresses are tracked to the same user_id
+    # (found in wikiconv-20**/speakers.json). Thus, to be safe, we consider
+    # them as separate, unique users
+    df.speaker = df.speaker.apply(
+        lambda x: (
+            x if not x.endswith("xxx") else f"unknown-user-{uuid.uuid4().int}"
+        )
+    )
+
+    # Filter out conversations with only one user commenting
+    valid_discussion_ids = get_valid_discussion_ids(df)
+    df = df[df.conversation_id.isin(valid_discussion_ids)]
+
+    tqdm.pandas(desc="Detecting language", leave=False)
+    df = df[df.text.progress_apply(is_english)]
+
     df = add_notes(df)
     df = add_meta_cols(df)
     df = conform_to_pefk(df)
-
-    # Filter out conversations with only one user commenting
-    discussion_indices = get_discussion_ids(df)
-
-    print(
-        f"Filtering out {len(discussion_indices)} out of {CHUNK_SIZE} comments"
-    )
-    df = df[df.conv_id.isin(discussion_indices)]
     return df
 
 
@@ -87,10 +97,21 @@ def conform_to_pefk(df):
     return df
 
 
-def get_discussion_ids(df):
-    user_counts = df.groupby("conv_id")["user"].nunique()
+def get_valid_discussion_ids(df):
+    user_counts = df.groupby("conversation_id")["speaker"].nunique()
     valid_discussions = user_counts[user_counts > 1]
     return valid_discussions.index.tolist()
+
+
+def is_english(text: str) -> bool:
+    """
+    Check if a given text is written in English.
+
+    :param text: string to check language of
+    :return: True if the text is in English, False otherwise
+    """
+    lang, prob = langid.classify(text)
+    return lang == "en" and prob >= 0.7
 
 
 if __name__ == "__main__":
