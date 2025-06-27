a0d65b6daf8570e243e605a64af1768b93165183
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-06-27 10:17:05 +0300

    remove legacy code

--- scripts/wikiconv.sh
+++ scripts/wikiconv.sh
@@ -4,14 +4,6 @@ DOWNLOAD_DIR="../downloads/wikiconv"
 ZIP_ROOT="$DOWNLOAD_DIR/datasets/wikiconv-corpus/corpus-zipped"
 URL="https://zissou.infosci.cornell.edu/convokit/datasets/wikiconv-corpus/"
 
-# Check if 18 directories exist and match wikiconv-2001 to wikiconv-2018
-EXPECTED_DIRS=18
-actual_count=$(find "$DOWNLOAD_DIR" -maxdepth 1 -type d -regextype posix-extended -regex ".*/wikiconv-20(0[1-9]|1[0-8])" | wc -l)
-if [ "$actual_count" -eq "$EXPECTED_DIRS" ]; then
-    echo "All 18 wikiconv directories already exist. Skipping download and unzip."
-    exit 0
-fi
-
 mkdir -p "$DOWNLOAD_DIR"
 wget --no-verbose -nc -r -nH --no-parent --cut-dirs=1 -P "$DOWNLOAD_DIR" "$URL" -A .zip
 
