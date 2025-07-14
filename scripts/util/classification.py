f4ae55e8d9cfd80d4902ece92aa5e5f3f02470df
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-07-14 13:22:10 +0300

    hehe

--- scripts/util/classification.py
+++ scripts/util/classification.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn
-from sklearn.model_selection import train_test_split
+import sklearn.model_selection
+import sklearn.metrics
 
 
 SEED = 42
@@ -28,11 +29,34 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
 
 
-def train_validate_test_split(
+def train_val_test_df(
+    df: pd.DataFrame, tokenizer, seed: int, max_length: int
+) -> pd.DataFrame:
+    train_df, val_df, test_df = _train_validate_test_split(
+        df,
+        stratify_col="is_moderator",
+        train_percent=0.75,
+        validate_percent=0.15,
+        seed=seed,
+    )
+
+    return train_df, val_df, test_df
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    preds = (logits.reshape(-1) > 0).astype(int)
+    return {
+        "accuracy": sklearn.metrics.accuracy_score(labels, preds),
+        "f1": sklearn.metrics.f1_score(labels, preds),
+    }
+
+
+def _train_validate_test_split(
     df, stratify_col, train_percent=0.6, validate_percent=0.2, seed=None
 ):
     # First split into train and temp (validate + test)
-    train, temp = train_test_split(
+    train, temp = sklearn.model_selection.train_test_split(
         df,
         stratify=df[stratify_col],
         test_size=1 - train_percent,
@@ -41,7 +65,7 @@ def train_validate_test_split(
 
     # Then split temp into validate and test
     validate_size = validate_percent / (1 - train_percent)
-    validate, test = train_test_split(
+    validate, test = sklearn.model_selection.train_test_split(
         temp,
         stratify=temp[stratify_col],
         test_size=1 - validate_size,
