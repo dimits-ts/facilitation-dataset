0787151c974a743ebf6ebccfd3038b6855de5e9b
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-09-24 12:49:51 +0300

    add new scripts to the taxonomy bash script

--- analyze_taxonomies.sh
+++ analyze_taxonomies.sh
@@ -6,3 +6,7 @@ xonomy.yaml --prompt_file=taxonomies/config/prompt.txt --output_dir=taxonomies/o
 od_probability_thres=0.6 --logs_dir=logs/taxonomies_llm | tee "../$LOG_FILE"
 
 python scripts/taxonomy_train.py --dataset_path pefk.csv --output_dir checkpoints/taxonomies --logs_dir=logs/taxonomies_training --labels_dir=taxonomies/output
+
+python scripts/taxonomy_inference.py --model_dir=checkpoints/taxonomies --dataset_path=pefk.csv --labels_dir=taxonomies/output --output_csv=output_datasets/taxonomies.csv
+
+python scripts/taxonomy_analysis.py --res_csv_path=logs/taxonomies_training/res.csv --graphs_dir=graphs
