0a9f1207c8c3b5a332d0660dceaa4c59279fca6a
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-09-25 15:33:48 +0300

    remove redundant log file for annotation

--- analyze_taxonomies.sh
+++ analyze_taxonomies.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-LOG_FILE="logs/pefk_taxonomies.log"
-
 python scripts/annotate_taxonomies.py --mod_probability_file=output_datasets/pefk_mod.csv --dataset_file=pefk.csv --taxonomy_file=taxonomies/config/ta
 xonomy.yaml --prompt_file=taxonomies/config/prompt.txt --output_dir=taxonomies/output --m
-od_probability_thres=0.6 --logs_dir=logs/taxonomies_llm | tee "../$LOG_FILE"
+od_probability_thres=0.6 --logs_dir=logs/taxonomies_llm
 
 python scripts/taxonomy_train.py --dataset_path pefk.csv --output_dir checkpoints/taxonomies --logs_dir=logs/taxonomies_training --labels_dir=taxonomies/output
 
