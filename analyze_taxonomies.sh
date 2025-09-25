3d5d72d4e248b5647180d9cb0642f2b3fae653ba
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-09-25 15:34:44 +0300

    oops

--- analyze_taxonomies.sh
+++ analyze_taxonomies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python scripts/annotate_taxonomies.py --mod_probability_file=output_datasets/pefk_mod.csv --dataset_file=pefk.csv --taxonomy_file=taxonomies/config/ta
-xonomy.yaml --prompt_file=taxonomies/config/prompt.txt --output_dir=taxonomies/output --m
+xonomy.yaml --prompt_file=taxonomies/config/prompt.json --output_dir=taxonomies/output --m
 od_probability_thres=0.6 --logs_dir=logs/taxonomies_llm
 
 python scripts/taxonomy_train.py --dataset_path pefk.csv --output_dir checkpoints/taxonomies --logs_dir=logs/taxonomies_training --labels_dir=taxonomies/output
