34fb7784b36be478223bd72411137aa710561466
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-09-30 15:12:05 +0300

    rename taxonomy annotation file

--- analyze_taxonomies.sh
+++ analyze_taxonomies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python scripts/annotate_taxonomies.py --mod_probability_file=output_datasets/pefk_mod.csv --dataset_file=pefk.csv --taxonomy_file=taxonomies/config/ta
+python scripts/taxonomy_annotate.py --mod_probability_file=output_datasets/pefk_mod.csv --dataset_file=pefk.csv --taxonomy_file=taxonomies/config/ta
 xonomy.yaml --prompt_file=taxonomies/config/prompt.json --output_dir=taxonomies/output --m
 od_probability_thres=0.6 --logs_dir=logs/taxonomies_llm
 
